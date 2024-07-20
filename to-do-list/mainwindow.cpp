374a14072f7fbb18841d4fe8c3e8526ad326a066
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2024-07-21 00:08:16 +0330

    consider tasks with clicking button and add it in list

--- to-do-list/mainwindow.cpp
+++ to-do-list/mainwindow.cpp
@@ -1,11 +1,45 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QListWidget>
+#include <QCheckBox>
+#include <QPushButton>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setWindowTitle("Add Task with Checkbox Example");
+
+        QLineEdit *lineEdit = new QLineEdit();
+        QListWidget *listWidget = new QListWidget();
+        QPushButton *button = new QPushButton("Add Task");
+
+        QVBoxLayout *layout = new QVBoxLayout();
+        layout->addWidget(lineEdit);
+        layout->addWidget(button);
+        layout->addWidget(listWidget);
+
+        connect(button, &QPushButton::clicked, [=]() {
+            QString task = lineEdit->text();
+            if (!task.isEmpty()) {
+                QCheckBox *checkBox = new QCheckBox(task);
+                listWidget->addItem("");
+                QListWidgetItem *item = listWidget->item(listWidget->count() - 1);
+                listWidget->setItemWidget(item, checkBox);
+                lineEdit->clear();
+            }
+        });
+
+        QWidget *centralWidget = new QWidget();
+        centralWidget->setLayout(layout);
+        setCentralWidget(centralWidget);
 }
 
 MainWindow::~MainWindow()
