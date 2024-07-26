e46505e10ada273a5852a40df4b839ffe16c348d
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2024-07-26 13:48:24 +0330

    draw a border between tasks in listWidget

--- to-do-list/mainwindow.cpp
+++ to-do-list/mainwindow.cpp
@@ -22,10 +22,12 @@ MainWindow::MainWindow(QWidget *parent)
     QLineEdit *lineEditTime = new QLineEdit();
     QListWidget *listWidget = new QListWidget();
     QPushButton *button = new QPushButton("Add Task");
+
     QDateTime dateTime = QDateTime::fromString("1.30.1", "M.d.s");
+
     dateTime = QDateTime::fromString("12", "yy");
 
-    QString dateTimeString = dateTime.toString()
+    QString dateTimeString = dateTime.toString();
 
     QVBoxLayout *layout = new QVBoxLayout();
     layout->addWidget(lineEdit);
@@ -35,12 +37,25 @@ MainWindow::MainWindow(QWidget *parent)
 
     connect(button, &QPushButton::clicked, [=]() {
         QString task = lineEdit->text();
-        lineEditTime->setText(date);
+        lineEditTime->setText(dateTimeString);
         if (!task.isEmpty()) {
             QCheckBox *checkBox = new QCheckBox(task);
             listWidget->addItem("");
             QListWidgetItem *item = listWidget->item(listWidget->count() - 1);
             listWidget->setItemWidget(item, checkBox);
+
+            // Create a frame to act as a border
+            QFrame *border = new QFrame();
+            border->setFrameShape(QFrame::HLine); // Horizontal line
+            border->setFrameShadow(QFrame::Sunken);
+            border->setLineWidth(1);
+            border->setStyleSheet("color: gray;");
+
+            // Add the border to the list widget
+            listWidget->addItem(""); // Add an empty item for the border
+            QListWidgetItem *borderItem = listWidget->item(listWidget->count() - 1);
+            listWidget->setItemWidget(borderItem, border);
+
             lineEdit->clear();
         }
     });
